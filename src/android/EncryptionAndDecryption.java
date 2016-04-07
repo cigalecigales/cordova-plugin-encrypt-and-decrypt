6c864a059180364e45998f3abc8f082e8b0a8fab
cigalecigales <cigalecigales@gmail.com>
2016-04-08 08:49:44 +0900

    Ver1.1.3 Fix import

--- src/android/EncryptionAndDecryption.java
+++ src/android/EncryptionAndDecryption.java
@@ -3,7 +3,7 @@ package plugin.en.de;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
-import plugin.en.de.EncAndDec;
+import plugin.en.de.EncryptionAndDescriptionUtil;
 
 public class EncryptionAndDecryption extends CordovaPlugin {
 

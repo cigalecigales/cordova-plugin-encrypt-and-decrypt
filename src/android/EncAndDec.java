b1c20bb4f6b6b914ddfb6cbb4ad3d398e3b05ff3
cigalecigales <cigalecigales@gmail.com>
2016-04-08 00:06:46 +0900

    Ver1.1.0 Add decrypt method

--- src/android/EncAndDec.java
+++ src/android/EncAndDec.java
@@ -17,21 +17,8 @@ public class EncAndDec {
 	 * @return encrypted message
 	 */
 	public static String encrypt(String key, String message) {
-		String encryptedMessage = null;
 		try {
-			// Get key's bytes length, and compare to MAX_SIZE.
-			int length = MAX_SIZE - key.getBytes("UTF-8").length;
-
-			// If key's bytes length is not MAX_SIZE
-			if (length > 0) {
-				StringBuffer buf = new StringBuffer(length);
-				for (int i = 0; i < length; i++) {
-					buf.append(ZERO);
-				}
-				key = key + buf.toString();
-			}else if(length < 0){
-				key = new String(key.getBytes("UTF-8"), 0, MAX_SIZE, "UTF-8");
-			}
+			key = adjustKeyLength(key);
 
 			byte[] byteText = message.getBytes("UTF-8");
 			byte[] byteKey = key.getBytes("UTF-8");
@@ -44,10 +31,8 @@ public class EncAndDec {
 
 			cipher.init(Cipher.ENCRYPT_MODE, enckey, iv);
 
-			// Base64 encoding
-			encryptedMessage = new String(Base64.encode(cipher.doFinal(byteText), Base64.DEFAULT));
-
-			return encryptedMessage;
+			// Return Base64 encoding message.
+			return new String(Base64.encode(cipher.doFinal(byteText), Base64.DEFAULT));
 
 		} catch (Exception ex) {
 			return null;
@@ -65,8 +50,7 @@ public class EncAndDec {
 		String decryptedMessage = null;
 
 		try {
-			// Base64
-			//byte[] byteText = Base64.decodeBase64(text);
+			key = adjustKeyLength(key);
 
 			byte[] byteKey = key.getBytes("UTF-8");
 			byte[] byteIv = key.getBytes("UTF-8");
@@ -74,17 +58,37 @@ public class EncAndDec {
 			SecretKeySpec deckey = new SecretKeySpec(byteKey, "AES");
 			IvParameterSpec iv = new IvParameterSpec(byteIv);
 
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 			cipher.init(Cipher.DECRYPT_MODE, deckey, iv);
 
-			byte[] byteResult = cipher.doFinal(message.getBytes("UTF-8"));
-
-			// バイト配列を文字列へ変換
-			decryptedMessage = new String(byteResult, "UTF-8");
-
-			return decryptedMessage;
+			// Return Decoded message.
+			return new String(cipher.doFinal(Base64.decode(message.getBytes("UTF-8"), Base64.DEFAULT)));
 		} catch(Exception ex){
 			return null;
 		}
+	}
+
+	/**
+	 * Adjust message length.
+	 *
+	 * @param key
+	 * @return adjusted message
+	 */
+	private static String adjustKeyLength(String key) throws UnsupportedEncodingException {
+		// Get key's bytes length, and compare to MAX_SIZE.
+		int length = MAX_SIZE - key.getBytes("UTF-8").length;
+
+		// If key's bytes length is not MAX_SIZE
+		if (length > 0) {
+			StringBuffer buf = new StringBuffer(length);
+			for (int i = 0; i < length; i++) {
+				buf.append(ZERO);
+			}
+			key = key + buf.toString();
+		} else if (length < 0) {
+			key = new String(key.getBytes("UTF-8"), 0, MAX_SIZE, "UTF-8");
+		}
+
+		return key;
 	}
 }
